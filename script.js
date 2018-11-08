edf593cb894ff1e7dcb3432400b1d5a0637cc216
Andrew Zhao <azhao@dmail.caltech.edu>
2018-11-07 20:47:04 -0800

    set timeout for new google change

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 var regex = /https?:\/\/(?:(?:www)|(?:encrypted))\.google(?:\.\w*){1,2}\/(?:(?:search)|(?:\?gws_rd=ssl)|(?:#q=)|(?:.*&q=)).*/;
+var timesFound = 0;
+var attempts = 0;
 if (regex.test(window.location.href)) {
-   delete window.wrappedJSObject.rwt
+  process();
+}
+function process() {
+  attempts ++;
+  if (!!window.wrappedJSObject.rwt) {
+    timesFound ++;
+    console.log('found ' + timesFound);
+    delete window.wrappedJSObject.rwt;
+	}
+  if (attempts > 200) {
+    return;
+  }
+  setTimeout(process, 50 + attempts);
 }
