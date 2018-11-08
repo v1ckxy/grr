beaaa0230296ac8cc9f5712a70b3680dd96b5883
Andrew Zhao <azhao@dmail.caltech.edu>
2018-11-07 19:57:24 -0800

    change to webextension

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-
 var regex = /https?:\/\/(?:(?:www)|(?:encrypted))\.google(?:\.\w*){1,2}\/(?:(?:search)|(?:\?gws_rd=ssl)|(?:#q=)|(?:.*&q=)).*/;
-if (regex.test(extension.getURL())) {
-   window.wrappedJSObject.rwt = 'slorgs';
+if (regex.test(window.location.href)) {
+   delete window.wrappedJSObject.rwt
 }
-
